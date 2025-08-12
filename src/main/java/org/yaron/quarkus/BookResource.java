c4cba1ac5e85179498e74fc4409ac031c49fb21b
yaronmag <yaron.magen@amdocs.com>
2025-08-12 13:50:16 +0300

    add BookRepository

--- src/main/java/org/yaron/quarkus/BookResource.java
+++ src/main/java/org/yaron/quarkus/BookResource.java
@@ -1,32 +1,30 @@
 package org.yaron.quarkus;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import org.yaron.quarkus.starting.Book;
+import org.yaron.quarkus.starting.BookRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Path("/api/books")
 public class BookResource {
-    // This class can be used to define REST endpoints for book-related operations
-    // For example, you can add methods to handle GET, POST, PUT, DELETE requests
-    // related to books in your application.
 
-    // Example method:
+        @Inject
+    BookRepository bookRepository;
+
      @GET
      @Path("/all")
      @Produces(MediaType.APPLICATION_JSON)
-     public List<Book> getAllBooks() {
-            Book book1 = new Book(1, "1984", "George Orwell", "Dystopian", 1949);
-            Book book2 = new Book(2, "To Kill a Mockingbird", "Harper Lee", "Fiction", 1960);
-            return List.of(book1, book2);
+        public List<Book> getAllBooks() {
+            return bookRepository.getAllBooks();
+        }
 
-         //return bookService.getAllBooks();
-     }
 
      @GET
      @Produces(MediaType.TEXT_PLAIN)
@@ -39,16 +37,18 @@ public class BookResource {
         @Path("/count")
         @Produces(MediaType.TEXT_PLAIN)
         public String countAllBooks() {
-         return "Total number of books: "+getAllBooks().size();
-     }
+            return bookRepository.countAllBooks();
+        }
+
 
      @GET
         @Path("/book/{id}")
         @Produces(MediaType.APPLICATION_JSON)
-        public Optional<Book> getBook(@PathParam("id") int id) {
-         return getAllBooks().stream().
-                 filter(book -> book.getId() == id)
-                 .findFirst();
-     }
+        public Optional<Book> getBookById(@PathParam("id") int id) {
+            return Optional.ofNullable(bookRepository.getBookById(id));
+        }
+
+
+
 }
 
